1ba61300857da158cd4e061db76e7910c82dc1e4
AKASH-SmartSoftwareLtd <akash.smartsoftware@yahoo.com>
2023-02-28 15:35:29 +0600

    add
    add

---
+++ module/PRM/database/migrations/2023_02_28_091602_create_parade_courses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParadeCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('parade_courses')){
+            Schema::create('parade_courses', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('course_id')->constrained();
+                $table->foreignId('parade_id')->constrained();
+                $table->string('serial_no')->nullable();
+                $table->string('duration')->nullable();
+                $table->foreignId('result')->nullable();
+                $table->foreignId('status')->default(1);
+                $table->timestamps();
+            });
+        }
+
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parade_courses');
+    }
+}
